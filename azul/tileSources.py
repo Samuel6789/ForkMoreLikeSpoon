0451dc54b3e4fe3ef85d9698cde2d5febf2c196a
Mich_H <60292048+Samuel6789@users.noreply.github.com>
2023-11-30 14:47:09 +0100

    Merge forgotten files

---
+++ azul/tileSources.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import List, Dict
+from simple_types import Tile, compress_tile_list, Points, RED, BLUE, YELLOW, GREEN, BLACK, STARTING_PLAYER
+
+class TileSource:
+    def __init__(self) -> None:
+        self.tiles: list[Tile] = []
+    
+    def isEmpty(self) -> bool:
+        return (self.tiles == [])
+    
+    def take(self, colour: int) -> List[Tile]:  #RBYGL
+        result: List[Tile] = []
+        mapColours: Dict[int, str] = {1 : "R", 2 : "B", 3 : "Y", 4 : "G", 5 : "L"}
+        i: int = 0
+        while i < len(self.tiles):
+            if colour < 0: 
+                return []
+            if mapColours[colour] == self.tiles[i].__str__():
+                result.append(self.tiles.pop(i))
+            else:
+                i += 1
+        return result
+    
+    def state(self) -> str:
+        return "".join([str(x) for x in self.tiles])
+
+
+class tableCenter(TileSource):
+    def __init__(self) -> None:
+        super().__init__()
+        self.tiles = [STARTING_PLAYER]
+
+    def add(self, tiles: list[Tile]) -> None:
+        self.tiles += tiles
+    
+    def take(self, colour: int) -> list[Tile]:  #RBYGL
+        result: list[Tile] = []
+        mapColours: Dict[int, str] = {0 : "S", 1 : "R", 2 : "B", 3 : "Y", 4 : "G", 5 : "L"}
+        i: int = 0
+        while i < len(self.tiles):
+            if colour < 0: 
+                return []
+            if self.tiles[i].__str__() == mapColours[colour] or self.tiles[i].__str__() == "S":
+                result.append(self.tiles.pop(i))
+            else:
+                i += 1
+        return result
